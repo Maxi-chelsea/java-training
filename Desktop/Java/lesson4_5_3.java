80feb25d9d1895b9378f1aa19e64a0c82a6d3cdd
Maxi-chelsea <maxi19950049@gmail.com>
2021-08-18 15:05:28 +0900

    lesson4_5_2

---
+++ Desktop/Java/lesson4_5_3.java
@@ -0,0 +1,14 @@
+public class lesson4_5_3 {
+  public static void main(String[] args) {
+    int ans1 = 0-4;
+    double ans2 = 3.14*2;
+    double ans3 = (double)5/3;
+    int ans4 = 30%7;
+    double ans5 = (7+32)/(double)5;
+    System.out.println(ans1);
+    System.out.println(ans2);
+    System.out.println(ans3);
+    System.out.println(ans4);
+    System.out.println(ans5);
+  }
+}
