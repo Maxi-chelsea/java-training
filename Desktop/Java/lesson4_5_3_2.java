5f058d27de04df4fc57d73206f41560acf641118
Maxi-chelsea <maxi19950049@gmail.com>
2021-08-18 15:15:11 +0900

    lesson4_5_3_2

---
+++ Desktop/Java/lesson4_5_3_2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class lesson4_5_3_2 {
+  public static void main(String[] args) throws IOException {
+    System.out.println("正方形の辺の長さを入力してください。");
+    BufferedReader br =
+      new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+    int width = Integer.parseInt(str);
+    System.out.println("正方形の面積は" + (width*width) + "です。");
+  }
+}
