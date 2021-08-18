738895625f30a612eef70a37d375143948fb1114
Maxi-chelsea <maxi19950049@gmail.com>
2021-08-18 14:53:19 +0900

    Sample22

---
+++ Desktop/Java/Sample22.java
@@ -0,0 +1,8 @@
+public class Sample22 {
+  public static void main(String[] args) {
+    int d = 2;
+    double pi = 3.14;
+    System.out.println("直径が" + d + "センチの円の");
+    System.out.println("円周は" + (d*pi) + "センチです。");
+  }
+}
