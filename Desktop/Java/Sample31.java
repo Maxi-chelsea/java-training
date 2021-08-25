401596aff586f7c281fc395f138904ab7f21e1c1
Maxi-chelsea <maxi19950049@gmail.com>
2021-08-25 16:04:47 +0900

    Sample29&30%31&32

---
+++ Desktop/Java/Sample31.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Sample31 {
+  public static void main(String[] args) throws IOException {
+    System.out.println("いくつまで合計を求めますか？");
+    BufferedReader br =
+      new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+    int num = Integer.parseInt(str);
+    int sum = 0;
+    for(int i=1; i <= num; i++) {
+      sum += i;
+    }
+    System.out.println("1から" + num + "までの合計は" + sum + "です。");
+  }
+}
