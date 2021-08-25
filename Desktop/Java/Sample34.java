c38ecdb34e176292a4fd8e067719ae96c909dc04
Maxi-chelsea <maxi19950049@gmail.com>
2021-08-25 16:18:16 +0900

    Sample33

---
+++ Desktop/Java/Sample34.java
@@ -0,0 +1,17 @@
+public class Sample34 {
+  public static void main(String[] args) {
+    boolean b1 = ;
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 5; j++) {
+        if (b1 == false) {
+          System.out.println("*");
+          b1 = true;
+        } else {
+          System.out.println("-");
+          b1 = false;
+        }
+      }
+      System.out.println("\n");
+    }
+  }
+}
